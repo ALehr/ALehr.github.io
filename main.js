81b6888e6efc6829a1dc3e2cbc26292fa9737e14
Anthony Lehr <anthony.lehr@gmail.com>
2025-08-08 13:06:58 -0400

    first pass at javascript column height for index.html

--- main.js
+++ main.js
@@ -1,10 +1,47 @@
-// handle two column layout on 1000px breakpoint
-const centerColumns = () => {
+/* console.log(`main height: ${document.querySelector('main').clientHeight}`);
+const sectionsArray = Array.from(document.querySelectorAll('main > *'));
+let sectionsHeight = sectionsArray.reduce( (total, el) => {return total + el.clientHeight}, 0 );
+sectionsHeight += 16 * 6;
+console.log(`sections height: ${sectionsHeight}`); */
 
+// handle two column layout on 1000px breakpoint
+const handleColumns = () => {
+
+    // main element is the flex container for the columns
     const $MAIN = document.querySelector('main');
 
     // verify breakpoint width
     if (document.querySelector('html').clientWidth >= 1000) {
+
+        // halve height for main to force flex columns to wrap
+        $MAIN.style.height = 'auto'; // reset before performing calculation
+        let columnsHeight = $MAIN.clientHeight / 2;
+        $MAIN.style.height = `${columnsHeight}px`;
+
+        // handle element that wraps onto third column
+        // find distance between 2nd to last element and bottom of body
+        const columnElements = Array.from(document.querySelectorAll('main > *'));
+        columnElements.sort((a, b) => getComputedStyle(a).order - getComputedStyle(b).order); // sorted by render order
+        
+        const overflowElement = columnElements[columnElements.length - 1];
+        const anchorElement = columnElements[columnElements.length - 2];
+        const col2Gap = $MAIN.getBoundingClientRect().bottom - anchorElement.getBoundingClientRect().bottom;
+        console.log(col2Gap);
+        console.log(overflowElement.clientHeight);
+        const neededHeight = overflowElement.clientHeight - col2Gap
+        console.log(neededHeight);
+        console.log(col2Gap + neededHeight);
+        console.log(columnsHeight);
+        columnsHeight += neededHeight + 32;
+        console.log(columnsHeight);
+        $MAIN.style.height = `${columnsHeight}px`;
+
+        // set precise main height to begin with
+            // halve main height
+            // use that value to find first element that will wrap onto 2nd column
+                // calculate height of 1st element to last before wrap
+                // calculate height of 1st wrap element to end
+                // set main height to larger of these two values (the longer of the two columns)
         
         // calculate width of body - left and right padding
         const $BODY = document.querySelector('body');
@@ -28,9 +65,10 @@ const centerColumns = () => {
         $MAIN.style.marginLeft = `${mainLeftMargin}px`;
 
     } else {
+        $MAIN.style.height = 'auto';
         $MAIN.style.marginLeft = 'auto';
     }
 }
 
-window.addEventListener('DOMContentLoaded', centerColumns);
-window.addEventListener('resize', centerColumns);
+window.addEventListener('DOMContentLoaded', handleColumns);
+window.addEventListener('resize', handleColumns);
